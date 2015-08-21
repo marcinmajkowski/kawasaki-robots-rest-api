c167aa108da22125fc067798bc99deb8d4625e0c
Marcin Majkowski <pl.marcin.m@gmail.com>
2015-08-21 19:37:01 +0200

    Put sample response to parse in parsing method comment

--- src/main/java/com/marcinmajkowski/kawasakirobotsrestapi/service/KawasakiRobotService.java
+++ src/main/java/com/marcinmajkowski/kawasakirobotsrestapi/service/KawasakiRobotService.java
@@ -26,19 +26,25 @@ public class KawasakiRobotService {
 
     public Location getToolCenterPoint() {
         String response = kawasakiTelnetClientService.query("where");
-        List<String> tokens = Arrays.asList(response.split("\\s+"));
-        for (int i = 0; i < tokens.size(); i++) {
-            System.out.println(i + " " + tokens.get(i));
-        }
-        double x = Double.parseDouble(tokens.get(19));
-        double y = Double.parseDouble(tokens.get(20));
-        double z = Double.parseDouble(tokens.get(21));
-        double o = Double.parseDouble(tokens.get(22));
-        double a = Double.parseDouble(tokens.get(23));
-        double t = Double.parseDouble(tokens.get(24));
+/* Response:
+where
+     JT1       JT2       JT3       JT4       JT5       JT6
+     0.000     0.000     0.000     0.000     0.000     0.000
+    X[mm]     Y[mm]     Z[mm]     O[deg]    A[deg]    T[deg]
+     0.000     0.000    80.000     0.000     0.000     0.000
+
+*/
+        //TODO following will work only if the robot has exactly 6 axis
+        String[] tokens = response.split("\\s+");
+        double x = Double.parseDouble(tokens[19]);
+        double y = Double.parseDouble(tokens[20]);
+        double z = Double.parseDouble(tokens[21]);
+        double o = Double.parseDouble(tokens[22]);
+        double a = Double.parseDouble(tokens[23]);
+        double t = Double.parseDouble(tokens[24]);
         double[] joints = new double[6];
         for (int i = 0; i < joints.length; i++) {
-            joints[i] = Double.parseDouble(tokens.get(7 + i));
+            joints[i] = Double.parseDouble(tokens[7 + i]);
         }
         return new Location("TCP", x, y, z, o, a, t, joints);
     }
